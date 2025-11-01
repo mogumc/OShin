fea8922043a25fbf3c6d6720d18b5af88a507995
mogumc <mogumc@outlook.com>
2025-11-01 13:28:36 +0800

    chore: init

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+import './App.css';
+// import {Greet} from "../wailsjs/go/main/App";
+
+function App() {
+    return (
+        <div id="App">
+        </div>
+    )
+}
+
+export default App
